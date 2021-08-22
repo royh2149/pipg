32051dd7ecbb477cfef9c7757df8306cf061e5f8
Roy Hatton <roy.hatton57@gmail.com>
2021-08-22 10:14:47 +0300

    Adding string representation of the victim

--- Victim.py
+++ Victim.py
@@ -1,5 +1,13 @@
-from typing import List
+def add_if_not_none(string: str, to_add: object, end: str, none_end: str, to_join=" ") -> str:
+	if to_add is not None:
+		if type(to_add) is list:
+			string += to_join.join(to_add) + end
+		else:
+			string += to_add.__str__() + end
+	else:
+		string += none_end
 
+	return string
 
 class Victim:  # the profile on which to base the generated passwords
 	def __init__(self):
@@ -9,10 +17,10 @@ class Victim:  # the profile on which to base the generated passwords
 		self.more_names = None
 		self.email = None
 		self.birth_date = None
-		self.pet = None
+		self.pets = None
 
 		# holds all the class variables
-		self.attrs = [self.first_name, self.last_name, self.more_names, self.email, self.birth_date, self.pet]
+		self.attrs = [self.first_name, self.last_name, self.more_names, self.email, self.birth_date, self.pets]
 
 	def generate_passwords(self) -> str:
 		keywords = []  # base keywords with which to generate the passwords
@@ -20,11 +28,25 @@ class Victim:  # the profile on which to base the generated passwords
 		# add all the filled params to the keywords
 		for param in self.attrs:
 			if param is not None:  # ensure the current param is filled
-				if type(param) is List:  # add all items in the more_names list
+				if type(param) is list:  # add all items in the more_names list
 					for val in param:
 						keywords.append(param)  # add the current attribute
 						keywords.append(param[::-1])  # add the current attribute, reversed
 				else:
 					keywords.append(param)  # add the current attribute
 					keywords.append(param[::-1])  # add the current attribute, reversed
+
+	def __str__(self) -> str:
+		# initialize the string
+		string = ""
+
+		# add all the attributes of the victim
+		string = add_if_not_none(string, self.first_name, " ", "")
+		string = add_if_not_none(string, self.more_names, " ", "", " ")
+		string = add_if_not_none(string, self.last_name, "\n", "\n")
+		string = add_if_not_none(string, self.email, "\n", "")
+		string = add_if_not_none(string, self.birth_date, "\n", "")
+		string = add_if_not_none(string, self.pets, "", "", ", ")
+
+		return string.strip()
 
