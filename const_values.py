2112f2af93efdbdb91b9771ddfbb6b4e54d0f961
Roy Hatton <roy.hatton57@gmail.com>
2021-08-28 17:33:28 +0300

    Updated constant values

---
+++ const_values.py
@@ -0,0 +1,27 @@
+from PyQt5.QtGui import QFont
+
+# error codes
+ARGS_ERROR = 1
+
+README_FILE = "README"
+
+EMPTY = None  # value of an empty field in the Victim class
+MAX_PASSWORD_LENGTH = 20  # maximum length of a password
+
+# command line arguments
+#args_funcs =
+# current argument type
+ARGUMENT = 1
+VALUE = 2
+
+# width and height of the graphical window
+WIDTH = 450
+HEIGHT = 450
+APP_NAME = "The Graphical Personal Information Password Generator"
+
+# font fot the title
+title_font = QFont()
+title_font.setBold(True)
+title_font.setUnderline(True)
+title_font.setPixelSize(30)
+
